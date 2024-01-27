c57fc5794a8484a60c3fe0f5b4280bc02d29e211
Sanskrip <sanaskrpk@gmail.com>
2024-01-27 03:49:57 +0200

    mobile modal fix

---
+++ js/modal-mobile.js
@@ -0,0 +1,8 @@
+const modalmobile = document.querySelector('.backdrop');
+const modalmobileBtnOpen = document.querySelector('.modal-btn-open-hero');
+const modalmobileBtnClose = document.querySelector('.modal-btn-close');
+
+const toggletablet = () => modal.classList.toggle('is-hidden');
+
+modalmobileBtnOpen.addEventListener('click', togglemodal);
+modalmobileBtnClose.addEventListener('click', togglemodal);
